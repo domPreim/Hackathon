7629dfd29aa456631df70527bb204d08e5cd1e5f
Bruzzknock <srbinsane@hotmail.rs>
2022-01-18 11:30:48 +0100

    monthly added

--- huskey-calculator/src/app/calculation.service.ts
+++ huskey-calculator/src/app/calculation.service.ts
@@ -10,7 +10,7 @@ export class CalculationService {
   //first array is always for non vacation procents. Second one is for the vacation procents
   lohnsteuerInfo = [[11000,18000, 31000, 60000, 90000, 1000000],[620,25000,50000,83333]];
   lohnsteuerProzent = [[0,20,35,42,48,50,55],[0,6,27,35.75,50]];
-  svInfo = [475.86,1790,1953,2117,5550];
+  svInfo = [6662.04,25060,27342,29638,77700];
   svSteuerProzent = [[0,15.12,16.12,17.12,18.12,1005.66],[0,14.12,15.12,16.12,17.12,1900.32]];
 
   //first small pendler then big pendler
@@ -37,12 +37,12 @@ export class CalculationService {
   }
 
   //returns NET salary.
-  netto(brutto: number,urlaub: number,kidsu18:number,kids18: number)
+  netto(brutto: number,urlaub: number,kidsu18:number,kids18: number,monatlich: boolean)
   {
-    return brutto - (this.lohnsteuer(brutto,urlaub,kidsu18,kids18)+this.sv(brutto,urlaub));
+    return monatlich ? (brutto/14) - (this.lohnsteuer(brutto,urlaub,kidsu18,kids18,monatlich)+this.sv(brutto,urlaub,monatlich)) :  brutto - (this.lohnsteuer(brutto,urlaub,kidsu18,kids18,monatlich)+this.sv(brutto,urlaub,monatlich));
   }
 
-  sv(brutto: number,urlaub: number)
+  sv(brutto: number,urlaub: number,monatlich: boolean)
   {
     var position =  0;
     var isPauschal = false;
@@ -59,15 +59,23 @@ export class CalculationService {
         isPauschal = true;
       }
     }
-    return isPauschal ? this.svSteuerProzent[urlaub][position] : brutto * this.svSteuerProzent[urlaub][position];
+
+    if(isPauschal)
+    {
+      return monatlich ? this.svSteuerProzent[urlaub][position] : this.svSteuerProzent[urlaub][position] * 14;
+    }
+    else
+    {
+      return monatlich ? (brutto/14) * this.svSteuerProzent[urlaub][position] : brutto * this.svSteuerProzent[urlaub][position];
+    }
   }
 
-  familienBonus(kidsu18: number,kids18: number)
+  familienBonus(kidsu18: number,kids18: number,monatlich: boolean)
   {
-    return kidsu18*125+kids18*41.68;
+    return monatlich ? kidsu18*125+kids18*41.68 : kidsu18*1500 + kids18*500.16;
   }
 
-  lohnsteuer(brutto: number,urlaub: number,kidsu18: number,kids18: number)
+  lohnsteuer(brutto: number,urlaub: number,kidsu18: number,kids18: number, monatlich: boolean)
   {
     var position =  0;
     for(var i = 0; i < this.lohnsteuerInfo[urlaub].length;i++)
@@ -83,7 +91,6 @@ export class CalculationService {
       }
     }
 
-    return brutto * this.lohnsteuerProzent[urlaub][position] - this.familienBonus(kidsu18,kids18);
+    return monatlich ? brutto / 14 * this.lohnsteuerProzent[urlaub][position] - this.familienBonus(kidsu18,kids18,monatlich) : brutto * this.lohnsteuerProzent[urlaub][position] - this.familienBonus(kidsu18,kids18);
   }
-
 }
